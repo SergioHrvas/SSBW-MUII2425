e8a938edcdf0ac8a963d73430b0ba2d22ab9d7e0
Sergio Hervás <sergiohervas18@gmail.com>
2025-03-21 12:57:02 +0100

    List of games dynamic advanced on index and search. It need to be paginated.

--- index.mjs
+++ index.mjs
@@ -1,15 +1,17 @@
 // index.mjs
-import express   from 'express'
-import nunjucks  from 'nunjucks'
+import express from 'express'
+import nunjucks from 'nunjucks'
 import gamesRouter from "./routes/games.mjs"
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
 
 const IN = process.env.IN || 'development' // development o production
 const app = express()
-			
+
 nunjucks.configure('views', {             // directorio 'views' para las plantillas html
 	autoescape: true,
-	noCache:    IN === 'development',       // true para desarrollo, sin cache
-	watch:      IN === 'development',       // reinicio con Ctrl-S
+	noCache: IN === 'development',       // true para desarrollo, sin cache
+	watch: IN === 'development',       // reinicio con Ctrl-S
 	express: app
 })
 
@@ -21,7 +23,7 @@ app.set('view engine', 'html')
 app.get('/hola', (req, res) => {          // test para el servidor
 	res.send('Hola desde el servidor');
 });
-	
+
 
 /* 
 app.get('/', (req, res) => {               // test plantillas en: 
@@ -32,8 +34,21 @@ app.get('/', (req, res) => {               // test plantillas en:
 
 //Tarea 1
 
-app.get('/', (req, res) => {
-	res.render("index.njk")
+app.get('/', async (req, res) => {
+	//Consultar juegos que tengan en su nombre la palabra buscada
+	const juegos = await prisma.juego.findMany({
+
+	})
+
+	try {
+		console.log(juegos[0])
+		res.render("index.njk", {juegos})
+
+	}
+	catch (err) {
+		console.error(err)
+		res.status(500).send({ err }) // o usar una página de error personalizada
+	}
 })
 
 app.get('/game', (req, res) => {
@@ -44,7 +59,7 @@ app.get('/game', (req, res) => {
 
 //Tarea 4
 
-app.use("/games", gamesRouter); 
+app.use("/games", gamesRouter);
 
 app.get('/buscar', (req, res) => {
 	res.render("search.njk")
